740e2a4f9129a173675eed9f1b569fdfa6651fdd
Hadi <abdulhady961@gmail.com>
2023-11-30 14:53:38 +0530

    first commit

---
+++ src/components/Head.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Image from "./components/Images/2.jpg"
+function Head(){
+    return(
+        <div>
+            <img src={Image} alt='noimage'/>
+            <img src="https://unsplash.com/photos/black-and-red-wireless-headphones-NrR9gn3lFKU"
+            alt='noimage'/>
+        </div>
+    )
+}
+export default Head
---
+++ src/components/Initialize.js
@@ -0,0 +1,6 @@
+// Create a file named initialize.js
+export const initializePlaylist = () => {
+    // Your initialization logic here
+    console.log('Playlist initialized');
+  };
+  
